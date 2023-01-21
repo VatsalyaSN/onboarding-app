0969da7241a3864353f9c9b500812164ed222364
v0n003b <vatsalya.nagendrappa@walmart.com>
2023-01-21 13:01:03 +0530

    Initial set up

---
+++ src/data/softwareList.js
@@ -0,0 +1,46 @@
+import notionLogo from '../assets/images/notion_logo.png';
+import azureLogo from '../assets/images/azure_logo.png';
+import jiraLogo from '../assets/images/jira_logo.jpeg';
+import noimicsLogo from '../assets/images/noimics_logo.png';
+import nopenSpeedLogo from '../assets/images/nopenSpeed_logo.png';
+import noyerBooksLogo from '../assets/images/noyerBooks_logo.png';
+import slackLogo from '../assets/images/slack_logo.png';
+
+
+export const softwareList = [
+    {
+        label: 'Notion',
+        value: 'notion',
+        icon: notionLogo
+    },
+    {
+        label: 'Azure',
+        value: 'azure',
+        icon: azureLogo
+    },
+    {
+        label: 'Jira',
+        value: 'jira',
+        icon: jiraLogo
+    },
+    {
+        label: 'Noimics',
+        value: 'noimics',
+        icon: noimicsLogo
+    },
+    {
+        label: 'Nopen Speed',
+        value: 'nopenSpeed',
+        icon: nopenSpeedLogo
+    },
+    {
+        label: 'Noyer Books',
+        value: 'noyerBooks',
+        icon: noyerBooksLogo
+    },
+    {
+        label: 'Slack',
+        value: 'slack',
+        icon: slackLogo
+    }
+]
